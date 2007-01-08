8d486e50424343a5aacc3a1ddbd7bd3ec0a6a00a
sir_gon <sir_gon@d8b663c7-2023-0410-b811-8a8d1591c0e1>
2007-01-08 22:07:29 +0000

    git-svn-id: svn://svn.code.sf.net/p/gdt/code/trunk@16 d8b663c7-2023-0410-b811-8a8d1591c0e1

--- demo20/CMakeFiles/gdtDemo.dir/DependInfo.cmake
+++
@@ -1,8 +0,0 @@
-# The set of files whose dependency integrity should be checked:
-SET(CMAKE_DEPENDS_LANGUAGES
-  "CXX"
-  )
-SET(CMAKE_DEPENDS_CHECK_CXX
-  "/home/gon/dev/libgdt/demo20/main.cpp"
-  "/home/gon/dev/libgdt/demo20/CMakeFiles/gdtDemo.dir/main.o"
-  )
--- demo20/CMakeFiles/gdtDemo.dir/cmake_clean.cmake
+++
@@ -1,4 +0,0 @@
-FILE(REMOVE
-"gdtDemo"
-"CMakeFiles/gdtDemo.dir/main.o"
-)
