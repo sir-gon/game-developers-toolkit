39ac9e8ce03a4a3c0d34d04d950d84ec3715999a
sir_gon <sir_gon@d8b663c7-2023-0410-b811-8a8d1591c0e1>
2007-07-16 18:12:18 +0000

    git-svn-id: svn://svn.code.sf.net/p/gdt/code/trunk@70 d8b663c7-2023-0410-b811-8a8d1591c0e1

---
+++ demos/skeleton/main.cpp
@@ -0,0 +1,39 @@
+#include <gdt.h>
+
+GD_Sistema Sistema;
+GD_Camara Camara;
+GD_Malla Figura;
+GD_Luz Luz;
+
+int main()
+{
+	Sistema.Inicializar(640,480,16,false,OPENGL);
+	Sistema.ColorFondo(30,30,30);
+
+	Camara.Crear(Tipo_Modelador);
+	
+	// Crea una Figura
+    Figura.CrearEsfera(30);
+    // Colores de la Figura
+    Figura.ColorEspecular(0,0,50);
+    Figura.ColorEmisivo(0,0,50);
+    Figura.ColorDifuso(0,0,50);
+    
+    // posicion // colores // radio
+    Luz.Crear(60,60,60, 30, 30, 30, 25);
+    
+    // Ciclo de dibujado
+	while(Sistema.EnEjecucion())
+	{
+          // Capturar Evento de Teclado
+          if (Sistema.TeclaAbajo(T_ESC))
+             // Cerrar Ejecucion
+             Sistema.Matarme();
+          // Dibujar
+          Sistema.Render();
+	}
+
+	Sistema.Matarme();
+
+	return 0;
+}
